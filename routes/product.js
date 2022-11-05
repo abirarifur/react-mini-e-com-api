6753bbc1ca677bfdc7dedb243204a4f2e93701e2
arifur <abirarifur@gmail.com>
2022-11-05 17:54:33 +0600

    user api done

--- routes/product.js
+++ routes/product.js
@@ -1,3 +1,95 @@
+const Product = require("../models/Product");
+const {
+  verifyToken,
+  verifyTokenAndAuth,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
+
 const router = require("express").Router();
+
+router.post("/", verifyTokenAndAdmin, async (req, res) => {
+  const newProduct = new Product(req.body);
+  try {
+    const savedProduct = await newProduct.save();
+    res.status(200).json(savedProduct);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.put("/:id", verifyTokenAndAuth, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASSWORD_SEC
+    ).toString();
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.delete("/:id", verifyTokenAndAuth, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("user has been deleted...");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new;
+
+  try {
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find();
+    // console.log(req.params.id);
+    // const { password, ...rest } = user._doc;
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+  try {
+    const data = await User.aggregate([
+      {
+        $match: {
+          createdAt: { $gte: lastYear },
+        },
+      },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
